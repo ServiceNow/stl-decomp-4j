766ab26055ba99d016061ec6dfa487ad6a7ddbf2
Jim Crotinger <jim.crotinger@servicenow.com>
2017-03-27 17:44:40 -0700

    First cut at a simple web demo app.

---
+++ examples/StlDemoRestServer/src/main/java/com/snc/stl4j/StlResults.java
@@ -0,0 +1,13 @@
+package com.snc.stl4j;
+
+/**
+ * Created by Jim Crotinger on 27-Mar-17.
+ */
+public class StlResults {
+	public long[] time;
+	public double[] value;
+	public double[] seasonal;
+	public double[] trend;
+	public double[] residual;
+	public double[] weight;
+}
